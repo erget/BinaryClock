2975273778d4f66cb8434b4118e76797c3ff2854
Daniel Lee <lee@isi-solutions.org>
2014-07-10 22:18:07 +0000

    First version that properly communicates with LED
    The output still goes to the wrong place and it looks like it's transposed, but this should be fixable.

--- src/main.cpp
+++ src/main.cpp
@@ -7,36 +7,48 @@
 #include "led-clock.hpp"
 #include "../external/rpi-rgb-led-matrix/led-matrix.h"
 
-// TODO: Remove, this is only for testing purposes
-class ColorPulseGenerator : public RGBMatrixManipulator {
+class LedClockMatrixManipulator : public RGBMatrixManipulator {
 public:
-  ColorPulseGenerator(RGBMatrix *m) : RGBMatrixManipulator(m) {}
+  LedClockMatrixManipulator(RGBMatrix *m) : RGBMatrixManipulator(m), clock() {}
+  ~LedClockMatrixManipulator() {
+    matrix_->ClearScreen();
+    matrix_->UpdateScreen();
+  }
+  // TODO: So far, so good. This now reports the right pixels (see console
+  // output, remembering that no output should be in the final method).
+  // However, it's not hitting the right positions on the LED. Get X and Y down
+  // and you will be very happy.
   void Run() {
-    const int width = matrix_->width();
-    const int height = matrix_->height();
-    uint32_t count = 0;
+    int i, r, g, b;
+    auto width = matrix_->width();
+    auto height = matrix_->height();
+    i = 0;
     while (running_) {
-      usleep(5000);
-      ++count;
-      int color = (count >> 9) % 6;
-      int value = count & 0xFF;
-      if (count & 0x100) value = 255 - value;
-      int r, g, b;
-      switch (color) {
-        case 0: r = value; g = b = 0; break;
-        case 1: r = g = value; b = 0; break;
-        case 2: g = value; r = b = 0; break;
-        case 3: g = b = value; r = 0; break;
-        case 4: b = value; r = g = 0; break;
-        default: r = g = b = value; break;
-      }
-      for (int x = 0; x < width; ++x)
-        for (int y = 0; y < height; ++y)
+      std::string current_time = clock.report_time();
+      for (int x = 0; x < width; ++x) {
+        for (int y = height; y > 0; --y) {
+          auto symbol = current_time[i];
+          switch (symbol) {
+            case '*': r = b = 0; g = 255; break;
+            case '#': g = b = 0; r = 255; break;
+            case '0': r = g = 0; b = 255; break;
+            case '1': r = g = 255; b = 0; break;
+            default: r = g = b = 0; break;
+          }
+          std::cout << symbol ;
           matrix_->SetPixel(x, y, r, g, b);
+          i++;
+        }
+      }
+      i = 0;
+      std::cout << "\n" << std::endl;
+      usleep(5000);
     }
   }
+private:
+  BinaryClock clock;
 };
-
+/*
 class LedClockMatrixManipulator : public RGBMatrixManipulator, public BinaryClock {
 public:
   LedClockMatrixManipulator(RGBMatrix *m) : RGBMatrixManipulator(m), BinaryClock() {}
@@ -68,11 +80,15 @@ public:
   usleep(5000);
   }
 };
+*/
 
+    /*
 class LedClock {
 public:
-  LedClock() : io(), matrix(NULL), manipulator(NULL), updater(NULL) {
+  //LedClock() : io(), matrix(NULL), manipulator(NULL), updater(NULL) {
+  LedClock() : io(){
     // TODO: Raise error if unsuccessful
+    std::cout << "Initializing LedClock" << std::endl;
     io.Init();
     RGBMatrix matrix(&io);
     LedClockMatrixManipulator manipulator(&matrix);
@@ -84,10 +100,11 @@ public:
   }
 private:
   GPIO io;
-  RGBMatrix matrix;
-  LedClockMatrixManipulator manipulator;
-  DisplayUpdater updater;
+  RGBMatrix * matrix;
+  LedClockMatrixManipulator * manipulator;
+  DisplayUpdater * updater;
 };
+    */
 
 int main(int argc, char *argv[]) {
     // TODO: Implement as something like this (works for displaying on CL)
@@ -95,18 +112,20 @@ int main(int argc, char *argv[]) {
 //	std::cout << clock.report_time() << std::endl;
 
     // TODO: Remove, this was just for testing and yes, it works
-    /*
     GPIO io;
     if (!io.Init()) return 1;
     RGBMatrix m(&io);
-    ColorPulseGenerator pulse_generator(&m);
+    LedClockMatrixManipulator pulse_generator(&m);
     DisplayUpdater updater(&m);
     updater.Start(10);
+    // TODO: This is causing problems
     pulse_generator.Start();
     getchar();
-    */
+    /*
     LedClock clock;
     clock.start();
+    std::cout << "D" << std::endl;
     getchar();
+    */
 	return 0;
 }
