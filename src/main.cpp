93f2d261675628dbda4965a0aae9b31ee82b0990
Daniel Lee <lee@isi-solutions.org>
2014-07-12 08:36:55 +0000

    First working version of clock
    It's not the prettiest code at the moment so I will definitely clean it up, but
    it works and is readable. Most of the functionality is still in main.cpp, but
    that will now change.

--- src/main.cpp
+++ src/main.cpp
@@ -14,34 +14,24 @@ public:
     matrix_->ClearScreen();
     matrix_->UpdateScreen();
   }
-  // TODO: So far, so good. This now reports the right pixels (see console
-  // output, remembering that no output should be in the final method).
-  // However, it's not hitting the right positions on the LED. Get X and Y down
-  // and you will be very happy.
   void Run() {
-    int i, r, g, b;
-    auto width = matrix_->width();
-    auto height = matrix_->height();
-    i = 0;
+    int x, y, r, g, b;
     while (running_) {
       std::string current_time = clock.report_time();
-      for (int x = 0; x < width; ++x) {
-        for (int y = height; y > 0; --y) {
-          auto symbol = current_time[i];
-          switch (symbol) {
-            case '*': r = b = 0; g = 255; break;
-            case '#': g = b = 0; r = 255; break;
-            case '0': r = g = 0; b = 255; break;
-            case '1': r = g = 255; b = 0; break;
-            default: r = g = b = 0; break;
-          }
-          std::cout << symbol ;
-          matrix_->SetPixel(x, y, r, g, b);
-          i++;
+      x = y = 0;
+      for (unsigned int i = 0; i < current_time.length(); ++i) {
+        auto symbol = current_time[i];
+        switch (symbol) {
+          case '*': r = b = 0; g = 255; break;
+          case '#': g = b = 0; r = 255; break;
+          case '0': r = g = 0; b = 255; break;
+          case '1': r = g = 255; b = 0; break;
+          case '\n': x = -1; y++; break;
+          default: r = g = b = 0; break;
         }
+        matrix_->SetPixel(x, y, r, g, b);
+        x++;
       }
-      i = 0;
-      std::cout << "\n" << std::endl;
       usleep(5000);
     }
   }
